a40f002b67a775ff3304aa067b0b887a217c6890
phong <vuducnghia15>
2019-09-14 03:01:04 -0700

    Turtle File

---
+++ conRua.py
@@ -0,0 +1,14 @@
+from turtle import *
+shape("turtle")
+color("purple")
+speed(0)
+
+
+for i in range (12):
+    for i in range(4): 
+        fd(100)
+        rt(90) 
+    rt(30)
+    
+    
+mainloop()
--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-a = int(input("Nhap vao nam sinh: "))
-b = input("nhap vao teen: ")
-print (2019 -(a))
-print("ban", 2019 -a ,"tuoi va ten la", b)
+# a = int(input("Nhap vao nam sinh: "))
+# b = input("nhap vao teen: ")
+# print (2019 -(a))
+# print("ban", 2019 -a ,"tuoi va ten la", b)
+a = 5
+b = 10
+print("a = ", a, "b = ", b)
+# c = a 
+# a = b
+# b = c
+a = a + b 
+b = a - b
+a = a - b 
+
+print(a,b)
+print("a = ", a, "b = ", b)
